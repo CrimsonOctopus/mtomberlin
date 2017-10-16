7a5002c15d3526e54d2e75168a2ccdf96a21c230
mtomberlin <mtomberlin@csumb.edu>
2017-10-16 22:16:38 +0000

    Lab5 part 3

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 function getDatabaseConnection(){
     $host = 'us-cdbr-iron-east-05.cleardb.net';//cloud 9
-    $dbname = 'tcp';
+    $dbname = 'heroku_0f5cc24af187a3f';
     $username = 'b3d9df8340a3c9';
     $password = '8f3e3255';
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
